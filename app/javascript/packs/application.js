8c74e4c527f23a6aa46af48c09209f6b16757f25
Mesut Ozil <matcortnermat@gmail.com>
2022-01-14 10:57:55 +0530

    Javascript added but timer functionality left

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,17 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+//= require jquery
+//= require jquery.turbolinks
+//= require jquery_ujs
+//= require turbolinks
+//= require bootstrap-sprockets
+//= require_tree
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+$(document).on("click", ".rounded-circle", function() {
+    alert("Hello");
+});
